c7ddb2393a9b8b525b0d68f9dd4cb74718ab216f
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2022-08-06 09:59:51 -0300

    Remove some files

---
+++ Moonshot/AstrounautView 2.swift
@@ -0,0 +1,36 @@
+//
+//  AstrounautView.swift
+//  Moonshot
+//
+//  Created by FABRICIO ALVARENGA on 06/08/22.
+//
+
+import SwiftUI
+
+struct AstrounautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstrounautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstrounautView(astronaut: astronauts["armstrong"]!)
+    }
+}
---
+++ Moonshot/Bundle-Decodable 2.swift
@@ -0,0 +1,32 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by FABRICIO ALVARENGA on 03/08/22.
+//
+
+import Foundation
+
+
+extension Bundle {
+    func decode<T>(_ file: String) -> T where T: Decodable {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+                       
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
